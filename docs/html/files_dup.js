10a8ba8afb0feda6cdf285b6b793d4a13e0ac1c4
Saloni Singh <62534764+saloni-singh14@users.noreply.github.com>
2025-12-12 12:30:54 +0530

    Last open files - one more directory left

---
+++ docs/html/files_dup.js
@@ -0,0 +1,5 @@
+var files_dup =
+[
+    [ "doc_pages", "dir_69c0ad47d15d71cfac8e0d206e4e350d.html", null ],
+    [ "source code", "dir_4cb6325948e15628ace1b41ec4dfca5a.html", "dir_4cb6325948e15628ace1b41ec4dfca5a" ]
+];
---
+++ docs/html/navtreedata.js
@@ -0,0 +1,66 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Whitted Ray tracing", "index.html", [
+    [ "Introduction", "index.html", [
+      [ "Turner-Whitted Ray tracing - breakthrough", "index.html#s1", [
+        [ "Ray tracing", "index.html#w1", null ],
+        [ "Need for simulating complex reflections and refractions", "index.html#w3", null ],
+        [ "What did he achieve?", "index.html#w2", null ]
+      ] ],
+      [ "What did he propose?", "index.html#propose", null ],
+      [ "References", "index.html#references", null ]
+    ] ],
+    [ "Output and timing analysis", "_output.html", [
+      [ "Cornell box", "_output.html#cornell", null ],
+      [ "Glass spheres", "_output.html#sky", null ],
+      [ "sky scene.", "_output.html#Night", null ]
+    ] ],
+    [ "Important features", "features.html", [
+      [ "Salient features of Whitted Ray-tracing", "features.html#hhhhh", [
+        [ "Recursivity", "features.html#f1", null ],
+        [ "Trees of rays", "features.html#f2", null ],
+        [ "Backward Tracing", "features.html#f3", null ]
+      ] ]
+    ] ],
+    [ "Implementation of Whitted ray tracing", "implementation.html", [
+      [ "Code explanation", "implementation.html#bvhghvhg", [
+        [ "Main Function", "implementation.html#main", null ],
+        [ "Radiance Function", "implementation.html#radiance", null ],
+        [ "Phong Illumination Model", "implementation.html#phong", null ],
+        [ "Intersection Calculation", "implementation.html#intersection", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_output.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
